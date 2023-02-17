ac5383280e4eb9379ba1adda7ad646b2ce8b022e
Lenovo <alexnguyen96@outlook.com>
2023-02-17 15:07:31 +1100

    Urgh, only the default from Github working. My decoder still not working

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from vit_pytorch import SimpleViT
 import torch
 from models import ViTSeg
 
+'''
+TODO:
+- Run to get 1 image working first
+- Use the weights from the Adapter
+- Try with smaller images like ADE first
+'''
+device = torch.device("cpu")
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
 
 v = ViTSeg(
-    image_size = 256,
-    patch_size = 32,
-    num_classes = 1000,
-    dim = 1024,
-    depth = 6,
-    heads = 16,
-    mlp_dim = 2048
+    image_size=256,
+    patch_size=32,
+    num_classes=1000,
+    dim=1024,
+    depth=6,
+    heads=16,
+    mlp_dim=2048,
+    dropout=0.1,
 )
 
-img = torch.randn(1, 3, 256, 256)
+img = torch.randn(4, 3, 256, 256)
 
-preds = v(img) # (1, 1000)
+preds = v(img)
 
-preds.show()
+preds
--- models.py
+++ models.py
@@ -6,6 +6,20 @@ from vit_pytorch.vit import Attention, FeedForward
 from vit_pytorch import ViT
 
 
+def xavier_init(module: nn.Module,
+                gain: float = 1,
+                bias: float = 0,
+                distribution: str = 'normal') -> None:
+    assert distribution in ['uniform', 'normal']
+    if hasattr(module, 'weight') and module.weight is not None:
+        if distribution == 'uniform':
+            nn.init.xavier_uniform_(module.weight, gain=gain)
+        else:
+            nn.init.xavier_normal_(module.weight, gain=gain)
+    if hasattr(module, 'bias') and module.bias is not None:
+        nn.init.constant_(module.bias, bias)
+
+
 class PixelShufflePack(nn.Module):
     """Pixel Shuffle upsample layer.
 
@@ -21,7 +35,7 @@ class PixelShufflePack(nn.Module):
             channels.
     """
 
-    def __init__(self, in_channels, out_channels, upsample_kernel, scale_factor=4):
+    def __init__(self, in_channels, out_channels, upsample_kernel=4, scale_factor=3):
         super(PixelShufflePack, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -35,37 +49,30 @@ class PixelShufflePack(nn.Module):
         self.init_weights()
 
     def init_weights(self):
-        nn.init.xavier_uniform_(self.upsample_conv)
+        xavier_init(self.upsample_conv, distribution='uniform')
 
     def forward(self, x):
         x = self.upsample_conv(x)
-        x = F.pixel_shuffle(x, self.scale_factor)
-        return x
-
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                Attention(dim, heads=heads, dim_head=dim_head),
-                FeedForward(dim, mlp_dim)
-            ]))
-            #TODO: add more linear layers to make the model deeper
-            #TODO: try using torch multihead attention instead of the Attention they made
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
+        x = F.pixel_shuffle(x, self.scale_factor)  # channel reduce by scale_factor**2, h&w increase by scale_factor
         return x
 
 class ViTSeg(ViT):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3, dim_head=64, dropout=0.1):
-        super().__init__(self, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3, dim_head=64, dropout=0.1)
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool='cls',
+                 channels=3, dim_head=64, dropout=0.1, emb_dropout = 0.1):
+        super(ViTSeg, self).__init__(image_size=image_size, patch_size=patch_size, num_classes=num_classes, dim=dim,
+                                     depth=depth, heads=heads, mlp_dim=mlp_dim, pool=pool, channels=channels,
+                                     dim_head=dim_head, dropout=dropout, emb_dropout=emb_dropout)
+        self.dropout_p = dropout
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, dim)
+        )
+        self.upsampler = PixelShufflePack(in_channels=dim, out_channels=num_classes)
+        self.linear_fuse = nn.Conv2d(dim, dim, (1, 1), bias=False)
+        self.bn = nn.BatchNorm2d(dim, eps=1e-5)
+        self.linear_pred = nn.Conv2d(dim, num_classes, kernel_size=(1, 1))
 
     def forward(self, img):
-        #TODO: change the code to work with the batch instead of just 1 image
         x = self.to_patch_embedding(img)
         b, n, _ = x.shape
 
@@ -75,12 +82,19 @@ class ViTSeg(ViT):
         x = self.dropout(x)
 
         x = self.transformer(x)
+        #TODO: make the transformer layer deeper
 
         x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
 
+        # modification starts here
         # x = self.to_latent(x)
         # x = self.mlp_head(x)
 
-        x = PixelShufflePack(x)
+        # x = self.upsampler(x)
+        x = self.linear_fuse(torch.cat(x, dim=1))
+        x = self.bn(x)
+        x = F.relu(x, inplace=True)
+        x = F.dropout(x, p=self.dropout_p)
+        x = self.linear_pred(x)
         return x
 
