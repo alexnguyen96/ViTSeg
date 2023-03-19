19fdb52afb05a76de1ca99ab7ae68508daf220db
Lenovo <alexnguyen96@outlook.com>
2023-03-20 00:36:34 +1100

    Waiting for meeting

--- encoder.py
+++ encoder.py
@@ -10,12 +10,12 @@ from segformer_aiden.segformer.backbones import mit_b0 as SegFormerEncoder #Aide
 from timm.models.vision_transformer import PatchEmbed, Block
 from helper import get_2d_sincos_pos_embed
 
+
 class MAEEncoder(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
     """
     def __init__(self, img_size=224, patch_size=16, in_chans=3,
                  embed_dim=1024, depth=24, num_heads=16,
-                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
                  mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
         super().__init__()
 
--- models.py
+++ models.py
@@ -6,18 +6,17 @@ import math
 from decoder import Decoder
 from encoder import SegFormerEncoder, MAEEncoder
 
+
 class VitSeg(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3,
                  embed_dim=1024, depth=24, num_heads=16,
                  decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
                  mlp_ratio=4., norm_layer=nn.LayerNorm):
         super().__init__()
-        self.mae_encoder = MAEEncoder(img_size, patch_size, in_chans,
-                 embed_dim, depth, num_heads,
-                 decoder_embed_dim, decoder_depth, decoder_num_heads,
-                 mlp_ratio, norm_layer)
+        self.mae_encoder = MAEEncoder(img_size, patch_size, in_chans, embed_dim,
+                                      depth, num_heads, mlp_ratio, norm_layer)
 
-        self.seg_encoder = SegFormerEncoder() #it's mit_b0 right now
+        self.seg_encoder = SegFormerEncoder()  # mit_b0 right now: (embed_dims=(32, 64, 160, 256), depths=(2, 2, 2, 2))
 
         # TODO: scale 4 times with scale factor 4 -> to get to 4k. Figure out the best out_channel num -> chatGPT?
         # TODO calculation to get the right img size in the end, -> find out outchanel, scalefact, kernel
@@ -25,20 +24,19 @@ class VitSeg(nn.Module):
         # self.decoder = Decoder(patch_num, num_classes, hidden_size, dropout=dropout, scale_factor=4)
 
     def forward(self, img):
-        x = self.seg_encoder(img)
-        for y in x:
-            print('y shape', y.shape)
-            # y shape torch.Size([1, 32, 64, 64])
-            # y shape torch.Size([1, 64, 32, 32])
-            # y shape torch.Size([1, 160, 16, 16])
-            # y shape torch.Size([1, 256, 8, 8])
-
         y = self.mae_encoder(img, mask_ratio=0.75)
         y = y[:, 1:, :]  # remove cls token
-        # y shape [1, 64, 256] [batch, 8*8, embed_dim]
+        # y shape: [1, 64, 1024] == [batch, 8*8, embed_dim]
 
-        #TODO: concat x and y -> z
-        # x = z
-        # x = self.decoder(x)
+        x = self.seg_encoder(img)
+        for layer in x:
+            print('layer shape', layer.shape)
+            # layer shape torch.Size([1, 32, 64, 64])
+            # layer shape torch.Size([1, 64, 32, 32])
+            # layer shape torch.Size([1, 160, 16, 16])
+            # layer shape torch.Size([1, 256, 8, 8])
 
-        return x
+            # TODO: make y and layer to have the same shape by changing embed_dim
+            #  then either concat or linear fuse the smallest one, then upsample and fuse with the next one?
+
+        return y
